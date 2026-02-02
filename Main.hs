a2797c741782145ac5d9ec3482e62063c27af861
mazerissa <youcefelghoul@yahoo.com>
2026-02-02 19:12:23 +0100

    Added reset to it & made it look better

--- Main.hs
+++ Main.hs
@@ -1,74 +1,78 @@
 import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Game
 import NeuralNet
+import Data.Fixed (mod')
 
-nodeRadius = 15
-layerGap   = 160
-nodeGap    = 40
-screenOff  = -320 
+rad = 15
+gapX = 160
+gapY = 40
+offX = -320 
 
-xorData = [ ([0,0], [0]), ([0,1], [1]), ([1,0], [1]), ([1,1], [0]) ]
-learningRate = 0.5
+dat = [ ([0,0], [0]), ([0,1], [1]), ([1,0], [1]), ([1,1], [0]) ]
+lr = 0.5 
+arch = [2, 8, 8, 4, 1]
 
-data State = State { net :: Network, history :: [Float], time :: Float }
+data State = State { net :: Network, history :: [Float], t :: Float }
 
-weightColor w = 
-    let intensity = realToFrac (min 1.0 (abs w))
+c w = 
+    let i = realToFrac (min 1.0 (abs w))
     in if w > 0 
-       then mixColors intensity (1 - intensity) (light cyan) (greyN 0.1)
-       else mixColors intensity (1 - intensity) (light orange) (greyN 0.1)
+       then mixColors i (1-i) cyan (greyN 0.1)
+       else mixColors i (1-i) orange (greyN 0.1)
 
-getPos lIdx nIdx totalNodes = 
-    ( fromIntegral lIdx * layerGap + screenOff
-    , fromIntegral nIdx * nodeGap - (fromIntegral totalNodes * nodeGap / 2)
+pos l n total = 
+    ( fromIntegral l * gapX + offX
+    , fromIntegral n * gapY - (fromIntegral total * gapY / 2)
     )
 
-drawSpark p1 p2 t =
-    let progress = mod' t 1.0
-        (x1, y1) = p1
-        (x2, y2) = p2
-        sx = x1 + (x2 - x1) * progress
-        sy = y1 + (y2 - y1) * progress
-    in translate sx sy $ color white $ circleSolid 3
-
-render (State currentNet errs t) = pictures [connections, sparks, nodes, graph, labels]
+render (State n h time) = pictures [wlines, sparks, nodes, plot, txt, results]
   where
-    architecture = [2, 8, 8, 4, 1]
-    prevCounts = init architecture
-    
-    connections = pictures $ zipWith3 drawLayer [0..] currentNet prevCounts
-    drawLayer lIdx layer prevT = pictures 
-        [ color (weightColor ((weights layer !! j) !! i)) $ 
-          line [getPos lIdx i prevT, getPos (lIdx+1) j (length (biases layer))]
-        | i <- [0..prevT-1], j <- [0..length (biases layer)-1] ]
+    prevs = init arch
 
-    sparks = pictures $ zipWith3 drawLayerSparks [0..] currentNet prevCounts
-    drawLayerSparks lIdx layer prevT = pictures
-        [ drawSpark (getPos lIdx i prevT) (getPos (lIdx+1) j (length (biases layer))) (t + fromIntegral (i+j))
-        | i <- [0..prevT-1], j <- [0..length (biases layer)-1]
-        , abs ((weights layer !! j) !! i) > 0.5 ]
+    wlines = pictures $ zipWith3 (\lIdx layer pCount -> 
+        pictures [ color (c ((weights layer !! j) !! i)) $ 
+                   line [pos lIdx i pCount, pos (lIdx+1) j (length (biases layer))]
+                 | i <- [0..pCount-1], j <- [0..length (biases layer)-1] ]) [0..] n prevs
+
+    sparks = pictures $ zipWith3 (\lIdx layer pCount ->
+        pictures [ let p1 = pos lIdx i pCount
+                       p2 = pos (lIdx+1) j (length (biases layer))
+                       prog = mod' (time * 0.7 + fromIntegral (i+j)) 1.0 
+                       (x1,y1) = p1
+                       (x2,y2) = p2
+                   in translate (x1+(x2-x1)*prog) (y1+(y2-y1)*prog) $ color white $ circleSolid 2
+                 | i <- [0..pCount-1], j <- [0..length (biases layer)-1]
+                 , abs ((weights layer !! j) !! i) > 0.8 ]) [0..] n prevs
 
     nodes = pictures $ zipWith (\l t -> pictures 
-        [ translate x y $ pictures [color white (circleSolid nodeRadius), color black (circle nodeRadius)]
-        | i <- [0..t-1], let (x, y) = getPos l i t ]) [0..] architecture
+        [ translate x y $ pictures [color (greyN 0.8) (circleSolid rad), color white (circle (rad+2))]
+        | i <- [0..t-1], let (x, y) = pos l i t ]) [0..] arch
 
-    graph = translate (-350) (-250) $ color green $ line $ 
-            zip [0, 2..] (take 350 $ map (* 200) errs)
+    plot = translate (-380) (-300) $ color (dim green) $ line $ zip [0, 2..] (map (*150) (take 400 h))
 
-    labels = translate (-350) 280 $ scale 0.12 0.12 $ color white $ 
-             text $ "Loss: " ++ show (if null errs then 0 else head errs)
+    txt = translate (-440) 330 $ scale 0.12 0.12 $ color white $ pictures
+          [ text $ "LOSS: " ++ show (if null h then 1.0 else head h)
+          , translate 0 (-200) $ text "Hit R to Reset the NN" 
+          ]
 
-update _ dt (State currentNet errs t) = 
-    let nextNet = iterate (trainDataset learningRate xorData) currentNet !! 10
-        currentError = sum [ (head (last (forward inp nextNet)) - head target) ** 2 
-                           | (inp, target) <- xorData ] / 4
-    in State nextNet (realToFrac currentError : take 500 errs) (t + dt)
+    results = translate 200 330 $ scale 0.12 0.12 $ color (light (light cyan)) $ pictures $
+        zipWith (\(i, _) idx -> translate 0 (fromIntegral idx * (-180)) $ 
+            text $ show i ++ " -> " ++ show (head (last (forward i n)))) dat [0..3]
 
-mod' x y = x - fromIntegral (floor (x / y)) * y
+handle (EventKey (Char 'r') Down _ _) s = do
+    newNet <- createRandomNetwork arch
+    return $ s { net = newNet, history = [] }
+handle _ s = return s
+
+step dt s = do
+    let next = iterate (trainDataset lr dat) (net s) !! 10
+        err = sum [ (head (last (forward i next)) - head t) ** 2 | (i, t) <- dat ] / 4
+    return $ s { net = next, history = realToFrac err : h, t = t s + dt }
+  where h = take 600 (history s)
 
 main :: IO ()
 main = do
-    
-    initialNet <- createRandomNetwork [2, 8, 8, 4, 1]
-    simulate 
-        (InWindow "Neural Net Live Brain" (900, 700) (50, 50))
-        (greyN 0.05) 60 (State initialNet [] 0) render update
+    putStrLn "Starting Neural Network Visualizer..."
+    initNet <- createRandomNetwork arch
+    playIO (InWindow "Neural Network In Haskell" (1100, 800) (10, 10))
+           (greyN 0.02) 60 (State initNet [] 0) (return . render) handle step
